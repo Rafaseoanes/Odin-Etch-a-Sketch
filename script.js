7ab18c7c0802927da6c7125d33cfe123cf336462
rafaseoanes <rafaseoanes9@gmail.com>
2023-11-13 13:05:34 -0500

    Styles updated

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 
 const gridContainer = document.querySelector("#grid-container")
+const clearBtn = document.querySelector("#clearBtn")
 
 
-
-    for(let i = 0; i < 15; i++){
-        const div = document.createElement("div")
-        div.classList.add("row")
-        div.classList.add("pixelStyle")
-        gridContainer.append(div)
-            for(let j = 0; j < 15; j++){
-                const div2 = document.createElement("div")
-                div2.classList.add("pixel")
-                div2.classList.add("pixelStyle")
-                div.append(div2)
-            }
+for (let i = 0; i < 16; i++) {
+    const div = document.createElement("div")
+    div.classList.add("row")
+    div.classList.add("pixelStyle")
+    gridContainer.append(div)
+    for (let j = 0; j < 16; j++) {
+        const div2 = document.createElement("div")
+        div2.classList.add("pixel")
+        div2.classList.add("pixelStyle")
+        div.append(div2)
     }
+}
 
-    const pixels = document.querySelectorAll(".pixel");
+const pixels = document.querySelectorAll(".pixel");
 
-    for(let pixel of pixels){
-        pixel.addEventListener("mouseover", function(){
-            pixel.style.backgroundColor = "black"
-        })
+for (let pixel of pixels) {
+    pixel.addEventListener("mouseover", function () {
+        pixel.style.backgroundColor = "black"
+    })
+}
+
+clearBtn.addEventListener("click", function () {
+
+    for (let pixel of pixels) {
+        pixel.style.backgroundColor = "white"
     }
-
-    
+})
