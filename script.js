99877aaa62cbcce6dfe8bcfd7a91223454e0e17e
rafaseoanes <rafaseoanes9@gmail.com>
2023-11-13 10:54:39 -0500

    First styles added, control bar added with no functionality yet

--- script.js
+++ script.js
@@ -6,9 +6,22 @@ const gridContainer = document.querySelector("#grid-container")
     for(let i = 0; i < 15; i++){
         const div = document.createElement("div")
         div.classList.add("row")
+        div.classList.add("pixelStyle")
         gridContainer.append(div)
             for(let j = 0; j < 15; j++){
                 const div2 = document.createElement("div")
+                div2.classList.add("pixel")
+                div2.classList.add("pixelStyle")
                 div.append(div2)
             }
     }
+
+    const pixels = document.querySelectorAll(".pixel");
+
+    for(let pixel of pixels){
+        pixel.addEventListener("mouseover", function(){
+            pixel.style.backgroundColor = "black"
+        })
+    }
+
+    
